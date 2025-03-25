5d7f26fdfabf6794d564f3b8701ba72774f27c5a
MengKorng00 <korngthyda6@gmail.com>
2025-03-26 01:56:47 +0700

    Add files via upload

--- k9music.py
+++ k9music.py
@@ -212,4 +212,4 @@ if __name__ == '__main__':
     web_thread.start()
 
     # Run the Discord bot.
-    bot.run("MTM0MzE3OTgxNDIwNjU3MDU5OQ.GkiBtj.GhPGhR_5PotgLfjXnAeEsPEqicfcd413gsYVKM")
+    bot.run("MTM0MzE3OTgxNDIwNjU3MDU5OQ.G8R5Mo.a_YcgNhRI-waRTgL_ul6KXEPE6lbR2gUUi8yrQ")
